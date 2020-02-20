1f670b054a7ff36a1eae6e21a6effe4dc4fd3b60
Qs-F <qsf@de-liker.com>
2020-02-20 15:44:35 +0900

    Update theme.ts

--- src/themec.ts
+++ src/themec.ts
@@ -6,16 +6,35 @@ export class Themec {
   subtheme: Color = Color('#00a050')
   whitish: Color = Color('#fff')
   blackish: Color = Color('#000')
-  customs: Array<Map<string, Custom>> = []
+  customs: Style = new Style()
 
-  constructor(theme: Color, whitish?: Color, blackish?: Color, subtheme?: Color) {
+  constructor(theme?: Color, whitish?: Color, blackish?: Color, subtheme?: Color) {
     if (window?.matchMedia('(prefers-color-scheme: dark)')?.matches) {
       this.mode = Mode.Dark
     }
-    this.theme = theme
+    this.theme = theme || this.theme
     this.subtheme = subtheme || this.subtheme
     this.whitish = whitish || this.whitish
     this.blackish = blackish || this.blackish
+  }
+
+  addCustom(name: string, variable: Variable | string) {
+    this.customs.setVariable(name, variable)
+  }
+
+  apply(element: HTMLElement) {
+    element.style.setProperty('--theme', this.theme.string())
+    element.style.setProperty('--subtheme', this.subtheme.string())
+    element.style.setProperty('--blackish', this.blackish.string())
+    element.style.setProperty('--whitish', this.whitish.string())
+
+    for (const [k, v] of Object.entries(this.customs.style)) {
+      element.style.setProperty('--' + k, v.toCSS())
+    }
+  }
+
+  color(col: string): Color{
+    return Color(col)
   }
 }
 
@@ -24,6 +43,30 @@ export enum Mode {
   Light
 }
 
-export interface Custom {
-  ToCSS(): string
+export interface Variable {
+  toCSS(): string
+}
+
+export class VariableImplement {
+  variable: string = ''
+
+  constructor(variable: string) {
+    this.variable = variable
+  }
+
+  toCSS(): string {
+    return this.variable
+  }
+}
+
+export class Style {
+  style: Map<string, Variable> = new Map<string, Variable>()
+
+  setVariable(name: string, variable: string | Variable) {
+    if (typeof variable === 'string') {
+      this.style.set(name, new VariableImplement(variable))
+    } else {
+      this.style.set(name, variable)
+    }
+  }
 }
