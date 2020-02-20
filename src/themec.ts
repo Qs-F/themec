cc983f878dde8d1da0775d5655ad317a68ca9d59
Qs-F <qsf@de-liker.com>
2020-02-20 19:22:04 +0900

    Add docs

--- src/themec.ts
+++ src/themec.ts
@@ -1,27 +1,51 @@
 import Color = require('color')
 
+export default Themec
+
+// Mode is used to define current browser mode.
+export enum Mode {
+  Dark,
+  Light
+}
+
+// Variable is an interface to define css variable.
+export interface Variable {
+  toCSS(): string
+}
+
+// Themec is main class
 export class Themec {
-  mode: Mode = Mode.Light
+  // mode is prefers-color-scheme wrapping. Automatically set.
+  // if the system does not support color scheme, then Mode.Light is set.
+  mode: Mode = Mode.Light 
+  
+  // theme, subtheme, whitish, blackish is commonly used, so set by default
   theme: Color = Color('#00a0c8')
   subtheme: Color = Color('#00a050')
   whitish: Color = Color('#fff')
   blackish: Color = Color('#000')
-  customs: Style = new Style()
 
-  constructor(theme?: Color, whitish?: Color, blackish?: Color, subtheme?: Color) {
+  customs: Style = new Style() // customs is the field for custom css variables.
+
+  constructor(theme?: Color, subtheme?: Color, whitish?: Color, blackish?: Color) {
+    // detect system color scheme, dark or light
     if (window?.matchMedia('(prefers-color-scheme: dark)')?.matches) {
       this.mode = Mode.Dark
     }
+
     this.theme = theme || this.theme
     this.subtheme = subtheme || this.subtheme
     this.whitish = whitish || this.whitish
     this.blackish = blackish || this.blackish
   }
 
+  // addCustom adds corresponding name and variable to this.customs.
+  // variable can be both of Variable and string.
   addCustom(name: string, variable: Variable | string) {
     this.customs.setVariable(name, variable)
   }
 
+  // apply set current theme, subtheme, whitish, blackish and all customs as css variable to given element.
   apply(element: HTMLElement) {
     element.style.setProperty('--theme', this.theme.string())
     element.style.setProperty('--subtheme', this.subtheme.string())
@@ -33,18 +57,11 @@ export class Themec {
     }
   }
 
+  // color returns Colro object from css color string.
+  // This is for the browser js environment, since Color is Node.js project.
   color(col: string): Color{
     return Color(col)
   }
-}
-
-export enum Mode {
-  Dark,
-  Light
-}
-
-export interface Variable {
-  toCSS(): string
 }
 
 export class VariableImplement {
